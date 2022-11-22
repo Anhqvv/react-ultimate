c24cd2304c9cc9956563691c5c90561619a4a822
Anhqvv <anhqvv2022@gmail.com>
2022-11-22 14:07:11 +0700

    #47 Design Header với Bootstrap Navigation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import "./App.scss";
-import Header from "./components/Header";
-import TableUser from "./components/TableUser";
-import Container from "react-bootstrap/Container";
-import Toastify from "./components/Toastify";
-import HomePage from "./components/HomePage";
-import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-
+import Header from "./components/Header/Header";
 function App() {
    return (
       <>
          <div className="app-container">
             <Header />
-            <Container>
-               <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="/users" element={<TableUser />} />
-                  <Route path="/login" element={<Login />} />
-               </Routes>
-            </Container>
-            {/* <TableUser /> */}
          </div>
-         <Toastify />
       </>
    );
 }
