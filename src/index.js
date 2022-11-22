c7af83f55985320991ed84bf1a404de4fd4be8c5
Anhqvv <anhqvv2022@gmail.com>
2022-11-22 20:42:54 +0700

    #51. Index Routes

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import reportWebVitals from "./reportWebVitals";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/Home/HomePage";
 
 ReactDOM.render(
    <React.StrictMode>
       <Router>
          <Routes>
             <Route path="/" element={<App />}>
+               <Route index element={<HomePage />} />
                <Route path="/users" element={<User />} />
                <Route path="/admins" element={<Admin />} />
             </Route>
